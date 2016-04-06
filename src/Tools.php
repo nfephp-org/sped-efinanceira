89d529e680de6c27ec8f6b517424862c74a9bd05
Roberto L. Machado <linux.rlm@gmail.com>
2016-04-06 18:21:08 -0300

    [UPD] testes unitários

--- src/Tools.php
+++ src/Tools.php
@@ -34,16 +34,13 @@ class Tools
         
     }
     
-    public function assina($xml, $tag)
-    {
-        
-    }
-    
+   
     /**
      * Monta lote de eventos para envio
      * se os eventos não estiverem assinados assina antes de montar o lote
      * @param array $aEv xml dos eventos a colocar no lote
-     * @param array $aResp variável passada como referencia irá conter os retornos em um array
+     * @param array $aResp variável passada como referencia irá conter os retornos
+     *                     em um array
      * @return string
      */
     public function enviaLote($aEv, &$aResp)
@@ -55,7 +52,10 @@ class Tools
         if ($num > 100) {
             throw new InvalidArgumentException("Somente podem ser mandados até 100 eventos por lote.");
         }
-        $lote = "<eFinanceira xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.eFinanceira.gov.br/schemas/envioLoteEventos/v1_0_1\">";
+        $lote = "<eFinanceira "
+                . "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" "
+                . "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+                . "xmlns=\"http://www.eFinanceira.gov.br/schemas/envioLoteEventos/v1_0_1\">";
         $lote .= "<loteEventos>";
         $iCount = 0;
         foreach ($aEv as $evento) {
