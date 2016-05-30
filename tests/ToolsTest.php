af3d8c54dee702bb68f82357656178f718904c2f
robmachado <linux.rlm@gmail.com>
2016-05-30 09:32:17 -0300

    [UPD] Ajuste na verificação de quando o certificado está vencido

--- tests/ToolsTest.php
+++ tests/ToolsTest.php
@@ -24,6 +24,7 @@ class ToolsTest extends FactoryTest
     
     /**
      * @covers NFePHP\eFinanc\Tools::__construct
+     * @expectedException RuntimeException
      */
     public function testInstantiable()
     {
