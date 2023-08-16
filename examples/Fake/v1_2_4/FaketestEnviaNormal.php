227fc1cfa46e501b99de2a6b1a7b4921556570f5
robmachado <linux.rlm@gmail.com>
2023-08-16 09:47:18 -0300

    atualização do certificado de encriptação de produção

--- examples/Fake/v1_2_4/FaketestEnviaNormal.php
+++ examples/Fake/v1_2_4/FaketestEnviaNormal.php
@@ -80,27 +80,27 @@ try {
     $content = file_get_contents('expired_certificate.pfx');
     $password = 'associacao';
     $certificate = Certificate::readPfx($content, $password);
-    
+
     //usar a classe Fake para não tentar enviar apenas ver o resultado da chamada
     $soap = new SoapFake();
-    //desativa a validação da validade do certificado 
+    //desativa a validação da validade do certificado
     //estamos usando um certificado vencido nesse teste
     $soap->disableCertValidation(true);
-    
+
     $evento = Event::evtAberturaeFinanceira($configJson, $std);
-    
+
     //instancia a classe responsável pela comunicação
     $tools = new Tools($configJson, $certificate);
     //carrega a classe responsável pelo envio SOAP
     //nesse caso um envio falso
     $tools->loadSoapClass($soap);
-    
+
     //executa o envio
     $response = $tools->enviar([$evento], $tools::MODO_NORMAL);
-    
+
     //retorna os dados que serão usados na conexão para conferência
     echo FakePretty::prettyPrint($response, '');
-    
+
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
