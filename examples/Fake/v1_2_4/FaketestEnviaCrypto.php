d043a0b60c33ca44d29f29a42ea4e1d64f7af9ef
robmachado <linux.rlm@gmail.com>
2023-11-27 16:13:12 -0300

    Alteração XSD EvtAberturaeFinanceira

--- examples/Fake/v1_2_4/FaketestEnviaCrypto.php
+++ examples/Fake/v1_2_4/FaketestEnviaCrypto.php
@@ -80,32 +80,33 @@ try {
     $content = file_get_contents('expired_certificate.pfx');
     $password = 'associacao';
     $certificate = Certificate::readPfx($content, $password);
-    
+
     //usar a classe Fake para não tentar enviar apenas ver o resultado da chamada
     $soap = new SoapFake();
-    //desativa a validação da validade do certificado 
+    $soap->timeout(60);
+    //desativa a validação da validade do certificado
     //estamos usando um certificado vencido nesse teste
     $soap->disableCertValidation(true);
-    
+
     $evento = Event::evtAberturaeFinanceira($configJson, $std);
-    
+
     //instancia a classe responsável pela comunicação
     $tools = new Tools($configJson, $certificate);
-    
+
     //carrega o certificado da RECEITA usado na encriptação das mensagens
     //$preprod = file_get_contents('preprod-efinanc_web.cer');
     //$tools->setCertificateEFinanceira($preprod);
-    
+
     //carrega a classe responsável pelo envio SOAP
     //nesse caso um envio falso
     $tools->loadSoapClass($soap);
-    
+
     //executa o envio
     $response = $tools->enviar([$evento], $tools::MODO_CRYPTO);
-    
+
     //retorna os dados que serão usados na conexão para conferência
     echo FakePretty::prettyPrint($response, '');
-    
+
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
